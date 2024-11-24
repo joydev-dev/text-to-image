43e927006dc56c7cecb7a4e238dd72d4ce4cab8f
joydev-dev <trisu1920@gmail.com>
2024-11-25 05:06:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const inputText = document.getElementById("input")
+const image = document.getElementById("image")
+const button = document.getElementById("btn")
+const token = "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"
+
+
+
+async function query(data) {
+   image.src = "/loader.gif"
+   const response = await fetch(
+      "https://api-inference.huggingface.co/models/ZB-Tech/Text-to-Image",
+      {
+         headers: {
+            Authorization: `Bearer ${token}`
+            
+         },
+         method: "POST",
+         body: JSON.stringify({ "inputs": inputText.value }),
+      }
+   );
+   const result = await response.blob();
+   return result;
+}
+button.addEventListener('click', async function () {
+   query().then((response) => {
+      const objectURL = URL.createObjectURL(response)
+      image.src = objectURL
+   });
+})
